d4ebae325f319680b376376304994e79cd062094
Maroo451 <marwan.alllam@gmail.com>
2026-04-11 19:18:43 +0200

    login

---
+++ login.js
@@ -0,0 +1,50 @@
+function login() {
+    let email = document.getElementById("EMAIL").value;
+    let name = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (name === "" || name.length < 5) {
+        alert("Name must be at least 5 characters long!");
+        return false;
+    }
+
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let valid = email.match(regex);
+    if (!valid) {
+        alert("Invalid email address!");
+        return false;
+    }
+
+    if (password === "" || password.length < 5) {
+        alert("Password must be at least 5 characters long!");
+        return false;
+    }
+
+    let userInfo = JSON.parse(localStorage.getItem('userInfo')) || [];
+
+    let matchedUser;
+    for (let i = 0; i < userInfo.length; i++) {
+        if (userInfo[i].email === email && userInfo[i].name === name && userInfo[i].password === password) {
+            matchedUser = userInfo[i];
+            break;
+        }
+    }
+
+    if (!matchedUser) {
+        alert("Invalid email, username, or password!");
+        return false;
+    }
+
+    localStorage.setItem("username", matchedUser.name);
+    localStorage.setItem("useremail", matchedUser.email);
+    localStorage.setItem("role", matchedUser.role);
+    localStorage.setItem("company", matchedUser.company);
+
+    if (matchedUser.role === "admin") {
+        window.location.href = "View_list.html";
+    } else {
+        window.location.href = "index.html";
+    }
+
+    return true;
+}
